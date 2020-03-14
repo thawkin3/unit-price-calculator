6f419c566c003dbe93ed05588da0959d98cef6ff
Tyler Hawkins <thawkin3@byu.net>
2020-03-14 15:33:01 -0600

    uses Promise.allSettled as part of the data fetching process

--- index.js
+++ index.js
@@ -10,4 +10,64 @@ express()
   .get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' })
   })
+  .get('/api/products', (req, res) => {
+    res.json({
+      products: [
+        { name: 'Widget', sku: 100 },
+        { name: 'Frozzle', sku: 101 },
+        { name: 'Gadget', sku: 102 },
+      ]
+    })
+  })
+  .get('/api/prices', (req, res) => {
+    res.json({
+      prices: [
+        {
+          sku: 100,
+          priceOptions: {
+            kg: [
+              '$200 for 10 kg',
+              '$300 for 20 kg',
+              '$1000 for 100 kg',
+            ],
+            lbs: [
+              '$200 for 22 lbs',
+              '$300 for 44 lbs',
+              '$1000 for 220 lbs',
+            ]
+          },
+        },
+        {
+          sku: 101,
+          priceOptions: {
+            kg: [
+              '$40 for 10 kg',
+              '$70 for 20 kg',
+              '$100 for 100 kg',
+            ],
+            lbs: [
+              '$40 for 22 lbs',
+              '$70 for 44 lbs',
+              '$100 for 220 lbs',
+            ]
+          },
+        },
+        {
+          sku: 102,
+          priceOptions: {
+            kg: [
+              '$15 for 2 kg',
+              '$17 for 3 kg',
+              '$20 for 5 kg',
+            ],
+            lbs: [
+              '$15 for 4 lbs',
+              '$17 for 6 lbs',
+              '$20 for 11 lbs',
+            ]
+          },
+        },
+      ]
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,48 @@
 (function(){
   console.log('hello world!')
 
-  // Use Promise.allSettled to fetch data from two API endpoints
-  // and then take an action once both endpoints have responded
-  const fetchInitialData = () => {
-    const promise1 = new Promise((resolve, reject) => setTimeout(() => resolve('promise 1 resolved!'), 100))
-    const promise2 = new Promise((resolve, reject) => setTimeout(() => resolve('promise 2 resolved!'), 200))
-
-    Promise.allSettled([promise1, promise2])
-      .then(data => console.log('all settled! here are the results:', data))
-      .catch(err => console.log('oh no, error! reason:', err))  
+  // Global state for our unit price calculator app
+  const appState = {
+    prices: [],
+    unitOfMeasurementPreference: null,
   }
 
+  // Use Promise.allSettled to fetch data from two API endpoints
+  // and then populate the product dropdown once both endpoints have responded
+  const fetchInitialData = () => {
+    const fetchProductsPromise = fetch('/api/products')
+      .then(response => response.json())
+
+    const fetchPricesPromise = fetch('/api/prices')
+      .then(response => response.json())
+
+    Promise.allSettled([fetchProductsPromise, fetchPricesPromise])
+      .then(data => {
+        console.log('all settled! here are the results:', data)
+        if (data?.[0]?.status === 'fulfilled' && data?.[0]?.status === 'fulfilled') {
+          const products = data[0].value?.products
+          const prices = data[1].value?.prices
+          populateProductDropdown(products)
+          savePriceData(prices)
+          return
+        }
+        throw new Error('Missing product or price data')
+      })
+      .catch(err => console.log('oh no, error! reason:', err))
+  }
+
+  const populateProductDropdown = (products = []) => {
+    const productDropdown = document.querySelector('#product')
+    const productDropdownOptions = products.reduce((optionHtml, product) => {
+      return `${optionHtml}<option value="${product.sku}">${product.name}</option>`
+    }, '')
+    productDropdown.insertAdjacentHTML('beforeend', productDropdownOptions)
+  }
+
+  const savePriceData = (prices = []) => {
+    appState.prices = prices
+  }
+
+  // Fetch some data when the page loads
   fetchInitialData()
 })()
