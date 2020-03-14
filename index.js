4f4b996528094e8f0bdb5e3301cc29d3d2f8570f
Tyler Hawkins <thawkin3@byu.net>
2020-03-14 14:06:58 -0600

    adds basic header to index.html page

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public' })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
